937589a96a9d02e0fbcde46ab31cee28ce0cf1ea
Vitalii Otverchenko <43891562+vitaliiotverchenko@users.noreply.github.com>
2022-03-20 14:51:24 +0200

    Add files via upload

---
+++ functions/Is Valid Triangle.py
@@ -0,0 +1,21 @@
+# Напишите функцию is_valid_triangle(side1, side2, side3), которая принимает в качестве аргументов три натуральных числа, и возвращает значение True если существует невырожденный треугольник со сторонами side1, side2, side3 и False в противном случае.
+#
+# Примечание 1. С данной задачей мы уже сталкивались при изучении условного оператора.
+#
+# Примечание 2. Следующий программный код:
+#
+# print(is_valid_triangle(2, 2, 2))
+# print(is_valid_triangle(2, 3, 10))
+# print(is_valid_triangle(3, 4, 5))
+# должен выводить:
+#
+# True
+# False
+# True
+
+def is_valid_triangle(side1, side2, side3):
+    return (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1)
+
+
+a, b, c = int(input()), int(input()), int(input())
+print(is_valid_triangle(a, b, c))
